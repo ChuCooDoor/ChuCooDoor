f68bc9eb84230e017abb567810fb42088251e5aa
Yuer <passerbyid@gmail.com>
2016-05-22 18:48:24 +0800

    Add multiple devices mode.

--- index.js
+++ index.js
@@ -6,155 +6,160 @@ var TelegramBot = require('node-telegram-bot-api');
 var config = require('./env.js');
 
 // init variables
-
-var boardPin = config.boardPin;
-var boardId = config.boardId;
+var devicesInfo = config.devicesInfo;
 var mqttBroker = config.mqttBroker;
 var telegram_token = config.telegram_token;
-var groupTitle = config.groupTitle;
-var groupChatId = config.telegram_groupChatId;
 var devGroupChatId = config.telegram_devGroupChatId;
-var board, lock, status, timer;
 
 var bot = new TelegramBot(telegram_token, {polling: true});
 
-// device info of webduino
-var deviceInfo = {
-  device: boardId,
-  server: mqttBroker
-};
-
-createWebArduino();
-
-board.on(webduino.BoardEvent.READY, onReady);
-board.on(webduino.BoardEvent.BEFOREDISCONNECT, onBeforeDisconnect);
-board.on(webduino.BoardEvent.DISCONNECT, onDisconnect);
-board.on(webduino.BoardEvent.ERROR, function (error) {
-    status = -1;
-    sendMessage(devGroupChatId, error);
-    log(error)
-    console.log('');
-});
-
-////////////////
-
-function createWebArduino() {
-  board = new webduino.WebArduino(deviceInfo);
+for (var index = 0; index < devicesInfo.length; index++) {
+  new ChuCooDoor(devicesInfo[index]);
 }
 
-function onReady() {
-  lock = new webduino.module.Button(board, board.getDigitalPin( boardPin ));
-  /*
-  ** `status` code list
-  ** -2: initial
-  ** -1: error
-  ** 0: close (same as board value)
-  ** 1: open (same as board value)
-  */
-  status = -2;
+function ChuCooDoor(deviceInfo) {
+  // deviceInfo = deviceInfo;
+  var board, lock, status, timer;
 
-  log('Ready');
-  console.log('');
+  // device info of webduino
+  var webduinoOption = {
+    device: deviceInfo.boardId,
+    server: mqttBroker
+  };
 
-  sendMessage(devGroupChatId, '開始監控');
+  createWebArduino();
 
-  // check status when device is on ready.
-  onCheck();
-
-  // check status when lock is triggered.
-  lock.on('pressed', onCheck);
-  lock.on('released', onCheck);
+  board.on(webduino.BoardEvent.READY, onReady);
+  board.on(webduino.BoardEvent.BEFOREDISCONNECT, onBeforeDisconnect);
+  board.on(webduino.BoardEvent.DISCONNECT, onDisconnect);
+  board.on(webduino.BoardEvent.ERROR, function (error) {
+      status = -1;
+      sendMessage(devGroupChatId, error, deviceInfo.groupTitle);
+      log(deviceInfo.groupTitle, error)
+      console.log('');
+  });
 
   ////////////////
 
-  function onCheck() {
-    // kill timer to prevent bad call(status of lock change too frequently).
-    if (timer) {
-      clearTimeout(timer);
-    }
+  function createWebArduino() {
+    board = new webduino.WebArduino(webduinoOption);
+  }
 
-    // set telegram group id.
-    if (status == 1 || status == 0) {
-      // someone switch the lock.
-      // sent to general group.
-      var chatId = groupChatId;
-    } else {
-      // check status at first.
-      // sent to dev group only.
-      var chatId = devGroupChatId;
-    }
+  function onReady() {
+    lock = new webduino.module.Button(board, board.getDigitalPin( deviceInfo.boardPin ));
+    /*
+    ** `status` code list
+    ** -2: initial
+    ** -1: error
+    ** 0: close (same as board value)
+    ** 1: open (same as board value)
+    */
+    status = -2;
 
-    // request for checking status
-    timer = setTimeout(check, 2000);
+    log(deviceInfo.groupTitle, 'Ready');
+    console.log('');
+
+    // sendMessage(devGroupChatId, '開始監控', deviceInfo.groupTitle);
+
+    // check status when device is on ready.
+    onCheck();
+
+    // check status when lock is triggered.
+    lock.on('pressed', onCheck);
+    lock.on('released', onCheck);
 
     ////////////////
 
-    function check() {
-      var boardValue = board.getDigitalPin(boardPin).value;
-
-      log('boardValue: ' + boardValue);
-
-      // prevent bad call(status of lock has not changed).
-      if (status != boardValue) {
-        log('Status: ' + status);
-
-        // prepare text for telegram bot.
-        var text = '';
-        if (boardValue == 1) {
-          text = '開門';
-        } else if (boardValue == 0) {
-          text = '關門';
-        }
-        // for initial check.
-        if (status == -2) {
-          text = text.concat('中');
-        }
-
-        // change status of lock.
-        status = boardValue;
-        sendMessage(chatId, text);
-        log(text);
-
-      } else {
-        log('忽略');
+    function onCheck() {
+      // kill timer to prevent bad call(status of lock change too frequently).
+      if (timer) {
+        clearTimeout(timer);
       }
-      console.log('');
+
+      // set telegram group id.
+      if (status == 1 || status == 0) {
+        // someone switch the lock.
+        // sent to general group.
+        var chatId = deviceInfo.telegram_groupChatId;
+      } else {
+        // check status at first.
+        // sent to dev group only.
+        var chatId = devGroupChatId;
+      }
+
+      // request for checking status
+      timer = setTimeout(check, 2000);
+
+      ////////////////
+
+      function check() {
+        var boardValue = board.getDigitalPin(deviceInfo.boardPin).value;
+
+        log(deviceInfo.groupTitle, 'boardValue: ' + boardValue);
+
+        // prevent bad call(status of lock has not changed).
+        if (status != boardValue) {
+          log(deviceInfo.groupTitle, 'Status: ' + status);
+
+          // prepare text for telegram bot.
+          var text = '';
+          if (boardValue == 1) {
+            text = '開門';
+          } else if (boardValue == 0) {
+            text = '關門';
+          }
+          // for initial check.
+          if (status == -2) {
+            text = text.concat('中');
+            text = '開始監控: ' + text;
+          }
+
+          // change status of lock.
+          status = boardValue;
+          sendMessage(chatId, text, deviceInfo.groupTitle);
+          log(deviceInfo.groupTitle, text);
+
+        } else {
+          log(deviceInfo.groupTitle, '忽略');
+        }
+        console.log('');
+      }
     }
+
   }
 
-}
+  function onBeforeDisconnect() {
+    log(deviceInfo.groupTitle, 'before disconnect');
+    console.log('');
+  }
 
-function onBeforeDisconnect() {
-  log('before disconnect');
-  console.log('');
-}
+  function onDisconnect() {
+    status = -1;
 
-function onDisconnect() {
-  status = -1;
+    sendMessage(devGroupChatId, 'ＧＧ', deviceInfo.groupTitle);
 
-  sendMessage(devGroupChatId, 'ＧＧ');
+    log(deviceInfo.groupTitle, 'disconnect');
+    console.log('');
 
-  log('disconnect');
-  console.log('');
-
-  setTimeout(createWebArduino, 5000);
+    setTimeout(createWebArduino, 5000);
+  }
 }
 
 bot.onText(/\/getId/, function (msg) {
-  var fromUsername = msg.from.username;
+  var fromUsername = msg.chat.title;
   var chatId = msg.chat.id;
 
-  sendMessage(devGroupChatId, chatId);
+  sendMessage(devGroupChatId, chatId, fromUsername);
 });
 
-function log(text) {
+function log(groupTitle, text) {
   var d = new Date();
   var date = d.toLocaleDateString();
   var time = d.toLocaleTimeString();
-  console.log(date + ' ' + time + ': ' + text);
+  console.log(date + ' ' + time + ': ' + groupTitle + ' ' + text);
 }
 
-function sendMessage(chatId, text) {
+function sendMessage(chatId, text, groupTitle) {
   var newText = '';
   if (chatId == devGroupChatId) {
     newText = groupTitle + ': ' + text;
